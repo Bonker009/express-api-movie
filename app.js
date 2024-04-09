3c23bc029551c06cd04417e8d04a72013ad084c8
seyha <penhseyha4980@gmail.com>
2024-04-09 10:03:30 +0700

    add endpoint get by id  and genre

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+const mockData1 = require("./scraped_data.json");
 let mockData = "";
 data = fs.readFile("scraped_data.json", "utf8", (error, data) => {
   if (error) {
@@ -14,6 +15,30 @@ const port = 3030;
 app.get("/", (req, res) => {
   const jsonData = JSON.parse(mockData);
   res.status(200).send({ payload: jsonData });
+});
+app.get("/search", (req, res) => {
+  const genre = req.query.genre;
+  console.log(genre);
+  if (!genre) {
+    res.status(400).json({ error: "genre parameter is required" });
+    return;
+  }
+
+  const results = mockData1.filter((movie) =>
+    movie.genre.toLowerCase().includes(genre.toLowerCase())
+  );
+  res.status(200).json(results);
+});
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const item = mockData1.find((movie) => movie.movie_id == id);
+  if (!item) {
+    res.status(404).json({
+      error: "Movie Not Found",
+    });
+  } else {
+    res.status(200).json(item);
+  }
 });
 
 app.listen(port, () => {
