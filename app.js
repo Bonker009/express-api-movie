f910b9c3775480d37be37e3036f2e807f1474ea2
seyha <penhseyha4980@gmail.com>
2024-04-09 13:18:56 +0700

    refactor response

--- app.js
+++ app.js
@@ -11,24 +11,21 @@ data = fs.readFile("scraped_data.json", "utf8", (error, data) => {
   mockData = data;
 });
 const port = 3030;
-
 app.get("/", (req, res) => {
-  const jsonData = JSON.parse(mockData);
-  res.status(200).send({ payload: jsonData });
-});
-app.get("/search", (req, res) => {
   const genre = req.query.genre;
   console.log(genre);
-  if (!genre) {
-    res.status(400).json({ error: "genre parameter is required" });
-    return;
-  }
 
-  const results = mockData1.filter((movie) =>
-    movie.genre.toLowerCase().includes(genre.toLowerCase())
-  );
-  res.status(200).json({ payload: results });
+  if (genre) {
+    const results = mockData1.filter((movie) =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+    res.status(200).json({ payload: results });
+  } else {
+    const jsonData = JSON.parse(mockData);
+    res.status(200).json({ payload: jsonData });
+  }
 });
+
 app.get("/:id", (req, res) => {
   const id = req.params.id;
   const item = mockData1.find((movie) => movie.movie_id == id);
@@ -41,6 +38,4 @@ app.get("/:id", (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log("server is running on port 3030");
-});
+app.listen(port);
